fc196e6e3b6e4ea04bd28a5983bb0ee20527c21c
ttkeele <85579834+ttkeele@users.noreply.github.com>
2022-02-25 10:46:53 -0600

    good ole fizz buzz explained in js

---
+++ fizzBuzz.js
@@ -0,0 +1,9 @@
+function fizzBuzz(num) {
+    for(let i = 1; i <= num; i++) {
+        if(i % 2 === 0 && i % 3 === 0) console.log("Fizz Buzz");
+         /* if the number is divisible by 2 or 3, display Fizz Buzz, must be checked first */
+         else if(i % 3 === 0) console.log("Buzz"); /* then check if divisible by 3 to print Buzz */
+         else if(i % 2 === 0) console.log("Fizz"); /* then check if num is divisible by 2 with 0 remainder to print Fizz */
+         else console.log(i) /* not divisible by 2 or 3 */
+    }
+}
