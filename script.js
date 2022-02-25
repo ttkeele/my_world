ce6b4ed3b4afe33c8c7bf8cda66601bfd2cd7e41
ttkeele <85579834+ttkeele@users.noreply.github.com>
2022-02-25 10:53:48 -0600

    oops need to make a script

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-function fizzBuzz(num) {
-    for(let i = 1; i <= num; i++) {
+function fizzBuzz(num)
+for(let i = 1; i <= num; i++) {
         if(i % 2 === 0 && i % 3 === 0) console.log("Fizz Buzz");
          /* if the number is divisible by 2 or 3, display Fizz Buzz, must be checked first */
          else if(i % 3 === 0) console.log("Buzz"); /* then check if divisible by 3 to print Buzz */
